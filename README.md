b6a031e68940ef102d35eb523e49f09db5b8d55e
ziga.povhe2 <ziga.povhe2@gmail.com>
2019-05-03 19:21:34 +0200

    Redesigned app, added notifications and fixed some bugs.

---
+++ README.md
@@ -0,0 +1,11 @@
+# PovheHome
+
+Controlling my home with the taps of my fingers. I use RaspberryPI as a server and a smart home controller.
+At this moment I can control lights, turn on my computer via WOL and open or close our garage with ESP8266.
+Expect an update with real-time sensor values
+
+
+<img src="https://i.imgur.com/ZqcZBSL.png" alt="app screenshot" height="700px"/>
+
+
+
--- app/src/main/java/si/povhe/povhehome/MainActivity.kt
+++ app/src/main/java/si/povhe/povhehome/MainActivity.kt
@@ -1,30 +1,36 @@
 package si.povhe.povhehome
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NotificationManagerCompat
 import android.util.Log
 import android.widget.Toast
-import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
-import si.povhe.povhehome.Services.AuthService
 
 class MainActivity : AppCompatActivity() {
+
+    val channelID = "RPI3_Channel"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lightBtn.setOnClickListener {
-            turnOnTheLight(this, "FkxYF#@!Zb25H5k"){ complete ->
-                if (complete) {
+        versionTxt.text = "v"+BuildConfig.VERSION_NAME
 
-                }
-            }
+        //Ce je SDK vecji od 26, nizje verzije androida bodo notification channel ignorirale
+        createNotificationChannel()
+
+        lightBtn.setOnClickListener {
+            turnOnTheLight(this, light_key)
         }
 
         zigapcBtn.setOnClickListener {
@@ -32,21 +38,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun turnOnTheLight(context: Context, auth:String, complete: (Boolean) -> Unit){
-        val url= URL_LIGHT
-
+    fun turnOnTheLight(context: Context, auth:String){
         val jsonBody = JSONObject()
         jsonBody.put("auth", auth)
         val requestBody = jsonBody.toString()
 
         val lightRequest = object : StringRequest(Method.POST, URL_LIGHT, Response.Listener { response ->
-            Toast.makeText(this, "$response", Toast.LENGTH_SHORT).show()
-            println(response)
-            complete(true)
+            Toast.makeText(this, response, Toast.LENGTH_SHORT).show()
+            //println(response)
+            sendNotification("LightSwitch",response)
         }, Response.ErrorListener { error ->
             Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
+            sendNotification("LightSwitch","Could not switch the light: $error")
             Log.d("ERROR", "Could not switch the light: $error")
-            complete(false)
         }) {
             override fun getBodyContentType(): String {
                 return "application/json; charset=utf-8"
@@ -61,17 +65,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun turnZigaPC(context: Context){
-        val url= URL_ZigaPC
-
         val jsonBody = JSONObject()
 
         val requestBody = jsonBody.toString()
 
         val zigaPCRequest = object : StringRequest(Method.GET, URL_ZigaPC, Response.Listener { response ->
-            Toast.makeText(this, "$response", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, response, Toast.LENGTH_SHORT).show()
+            sendNotification("ZigaPC", response)
             println(response)
         }, Response.ErrorListener { error ->
             Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
+            sendNotification("ZigaPC", "Could not turn on zigapc: $error")
             Log.d("ERROR", "Could not turn on zigapc: $error")
         }) {
             override fun getBodyContentType(): String {
@@ -86,4 +90,39 @@ class MainActivity : AppCompatActivity() {
         Volley.newRequestQueue(context).add(zigaPCRequest)
     }
 
+
+    //Notifications
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun sendNotification(title:String, content:String) {
+        var notificationID = 1
+
+        val builder = Notification.Builder(this, channelID)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setSmallIcon(R.mipmap.icon)
+            .setChannelId(channelID)
+
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationID++, builder.build())
+        }
+
+
+    }
+
 }
+
